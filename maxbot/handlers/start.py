24671d5cfa25b09e161556dcb02e17cc187bc9ba
Fotlex <kudron02@mail.ru>
2026-04-07 22:00:49 +1000

    first start

--- maxbot/handlers/start.py
+++ maxbot/handlers/start.py
@@ -1,11 +1,20 @@
 from asgiref.sync import sync_to_async
-from maxapi.types import MessageCreated, Command, MessageCallback
+from maxapi.types import MessageCreated, Command, MessageCallback, BotStarted
 from maxapi import F, Router
 from maxapi.context import MemoryContext
 from web.panel.models import AllText
 from maxbot.keyboards.inline import get_main_keyboard
 
 router = Router()
+
+
+@router.bot_started()
+async def bot_started(event: BotStarted):
+    text = await sync_to_async(AllText.objects.first)()
+    main_page_text = text.main_page if text else "Главное меню"
+    
+    await event.message.answer(text=main_page_text, attachments=[get_main_keyboard(text)])
+
 
 @router.message_created(Command('start'))
 async def start_handler(event: MessageCreated, context: MemoryContext):
